e87660ff199dfcc96ff21670b02bdc94ddfed6a1
adshriniwas <pawarshri7@gmail.com>
2023-03-18 16:02:07 +0530

    fragment set

--- app/src/main/java/com/shriniwas/pawar/videoplayer/MainActivity.kt
+++ app/src/main/java/com/shriniwas/pawar/videoplayer/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.shriniwas.pawar.videoplayer
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.shriniwas.pawar.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +21,62 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.coolPinkNav)
         setContentView(binding.root)
+//        requestRuntimePermission()
+        setFragment(VideosFragment())
 
         binding.bottomNav.setOnItemSelectedListener {
-            Toast.makeText(this@MainActivity, "Item Clicked.", Toast.LENGTH_SHORT).show()
+            when(it.itemId){
+                R.id.videoView -> setFragment(VideosFragment())
+                R.id.foldersView -> setFragment(FoldersFragment())
+            }
             return@setOnItemSelectedListener true
         }
     }
+
+    private fun setFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentFL, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+    }
+
+
+
+//    private fun requestRuntimePermission(): Boolean {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                ActivityCompat.requestPermissions(
+//                    this,
+//                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+//                    13
+//                )
+//                return false
+//
+//            }
+//        }
+//        return true
+//
+//    }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        if (requestCode == 13){
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show()
+//            }else {
+//                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+//            }
+//        }
+//    }
 }
